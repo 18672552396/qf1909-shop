559bde7b9714de2504d7579f78ebf79f2028c7a4
18672552396 <61958080+18672552396@users.noreply.github.com>
2020-04-24 15:22:39 +0800

    demo

---
+++ qf/App.java
@@ -0,0 +1,18 @@
+package com.qf;
+
+import com.sun.org.apache.bcel.internal.util.ClassPath;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Hello world!
+ */
+
+@Configuration("classpath:spring.xml")
+public class App {
+
+
+    public void test1() {
+
+
+    }
+}
